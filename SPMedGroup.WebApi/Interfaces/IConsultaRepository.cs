5936298ec5896fcdb52a67eab7d17497cae3d5ed
Luana Clementino de Moura <46863229870@info.local>
2019-03-12 11:21:16 -0300

    Começando consultas

---
+++ SPMedGroup.WebApi/Interfaces/IConsultaRepository.cs
@@ -0,0 +1,20 @@
+﻿using SPMedGroup.WebApi.Domains;
+using System.Collections.Generic;
+
+namespace SPMedGroup.WebApi.Interfaces
+{
+    interface IConsultaRepository
+    {
+        void Cadastrar(Consulta consulta);
+
+        void Editar(int id, Consulta consulta);
+
+        void CancelarConsulta(int id);
+
+        List<Consulta> ListarTodas();
+
+        List<Consulta> ListardoMedico();
+
+        List<Consulta> ListardoPaciente();
+    }
+}
