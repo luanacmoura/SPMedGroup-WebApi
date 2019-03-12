c4eb598809a7a8b4505857de4110b6f1ba8a9dc8
Luana Moura <luanacm@outlook.com.br>
2019-03-12 18:51:54 -0300

    problemas com sql personal pc

---
+++ SPMedGroup.WebApi/Controllers/ConsultaController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SPMedGroup.WebApi.Domains;
+using SPMedGroup.WebApi.Interfaces;
+using SPMedGroup.WebApi.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPMedGroup.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    class ConsultaController : ControllerBase
+    {
+        private IConsultaRepository ConsultaRepository { get; set; }
+
+        public ConsultaController() {
+            ConsultaRepository = new ConsultaRepository();    
+        }
+
+        [Authorize (Roles = "1")]
+        [HttpPost("{Cadastrar}")]
+        public IActionResult Post(Consulta consulta)
+        {
+            try
+            {
+                ConsultaRepository.Cadastrar(consulta);
+                return Ok("Consulta cadastrada com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Algo deu errado :/");
+            }
+        }
+    }
+}
---
+++ SPMedGroup.WebApi/Repositories/ConsultaRepository.cs
@@ -0,0 +1,48 @@
+﻿using SPMedGroup.WebApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SPMedGroup.WebApi.Domains;
+
+namespace SPMedGroup.WebApi.Repositories
+{
+    class ConsultaRepository : IConsultaRepository
+    {
+        public void Cadastrar(Consulta consulta)
+        {
+            using (SpMedGroupContext ctx = new SpMedGroupContext())
+            {
+                ctx.Consulta.Add(consulta);
+                ctx.SaveChanges();
+            }
+        }
+
+        public void CancelarConsulta(int id)
+        {
+            using (SpMedGroupContext ctx = new SpMedGroupContext())
+            {
+                
+            }
+        }
+
+        public void Editar(int id, Consulta consulta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Consulta> ListardoMedico()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Consulta> ListardoPaciente()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Consulta> ListarTodas()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
