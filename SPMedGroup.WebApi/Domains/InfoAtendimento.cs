9a09f85527a9b31d2584e3d74e809797b18d44d7
Luana Moura <luanacm@outlook.com.br>
2019-06-07 09:52:32 -0300

    Add required's

--- SPMedGroup.WebApi/Domains/InfoAtendimento.cs
+++ SPMedGroup.WebApi/Domains/InfoAtendimento.cs
@@ -1,6 +1,7 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,11 +16,11 @@ namespace SPMedGroup.WebApi.Domains
         public string Latitude { get; set; }
         [BsonElement("longitude")]
         public string Longitude { get; set; }
-
+        [Required(ErrorMessage = "Insira o id da área clínica!")]
         public int IdAreaClinica { get; set; }
 
         public AreaClinica IdAreaClinicaNavigation { get; set; }
-
+        [Required(ErrorMessage = "Insira a idade do paciente!")]
         public int IdadePaciente { get; set; }
     }
 }
